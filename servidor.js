9dcbc31591cc9ae8a2ef55f22f58552caf7afdde
rafafonsecaa <62483799+rafafonsecaa@users.noreply.github.com>
2022-06-08 19:25:45 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -20,17 +20,62 @@ var jogadores = {
 // Dispara evento quando jogadores entrarem na partida
 io.on("connection", function (socket) {
     if (jogadores.primeiro === undefined) {
-    jogadores.primeiro = socket.id;
+
+      var jogador = {
+        nome: socket.id,
+        id_sala: "labirintoCegoSala",
+        dono_sala: true
+      }
+
+      jogadores.primeiro = jogador;
+      socket.join(jogador.id_sala);       // criando a sala, dono: primeiro a entrar
+      
+      io.emit("created room", jogador);
 
   } else if (jogadores.segundo === undefined) {
-    jogadores.segundo = socket.id;
     
+    var jogador = {
+      nome: socket.id,
+      id_sala: "labirintoCegoSala",
+      dono_sala: false
+    }
+
+    jogadores.segundo = jogador;
+    // se jogador possui o acesso a sala, entra na partida 
+    socket.join(jogador.id_sala);
+
   } else if (jogadores.terceiro === undefined) {
-    jogadores.terceiro = socket.id;
+
+    var jogador = {
+      nome: socket.id,
+      id_sala: "labirintoCegoSala",
+      dono_sala: false
+    }
+
+    jogadores.terceiro = jogador;
+    // se jogador possui o acesso a sala, entra na partida 
+    socket.join(jogador.id_sala);
+
+  } else {
+    // sala cheia
+    io.emit("sala_cheia");
   }
-  io.emit("jogadores", jogadores);
+
+  io.emit("jogadores", jogadores)         // lista de jogadores enviada p geral
   console.log("+Lista de jogadores: %s", jogadores);
 
+  socket.on("retorno_sala_cheia", () => {              // resposta do emit feito no cliente --> 
+    console.log("status: 403")
+  });
+
+  socket.on("retorno_sucessfull", () => {              // resposta do emit feito no cliente --> 
+    console.log("status: 200")
+  });
+
+  socket.on("retorno_unsucessfull", () => {              // resposta do emit feito no cliente --> 
+    console.log("status: 401")
+  });
+  
   // Sinalização de áudio: oferta
   socket.on("offer", (socketId, description) => {
     socket.to(socketId).emit("offer", socket.id, description);
@@ -45,16 +90,6 @@ io.on("connection", function (socket) {
   socket.on("candidate", (socketId, signal) => {
     socket.to(socketId).emit("candidate", signal);
   });
-  
-  // Registro de jogadores na partida
-  //socket.on("Register, function () {
-   //     socket.broadcast.emit()
-  //          }")
-      
-  // Registro de jogadores na partida
-  //socket.on("Register, function () {
-  //      socket.broadcast.emit()
-  //          }")
 
   // Disparar evento quando jogador sair da partida
   socket.on("disconnect", function () {
