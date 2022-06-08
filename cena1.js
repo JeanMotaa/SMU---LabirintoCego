57ee50d67ee1ae3b8a343451c35610b50fdec8d6
rafafonsecaa <62483799+rafafonsecaa@users.noreply.github.com>
2022-06-08 11:16:43 -0300

    Update cena1.js

--- cena1.js
+++ cena1.js
@@ -5,28 +5,53 @@ import { cena2 } from "./cena2.js";
 var cena1 = new Phaser.Scene("Cena 1");
 
 // Variáveis locais
-
 var player1;
 var player2;
 
 var jogador;
 
-var localConnection;
-var remoteConnection;
-
 cena1.created = function () {
   
-   // Conectar no servidor via WebSocket
-  this.socket = io();
+  // Conectar no servidor via WebSocket
+  socket = io();
   
   // Disparar evento quando jogador entrar na partida
-  var self = this;
-  var socket = this.socket;
+  // var self = this;
+  // var socket = this.socket;
   
-  this.socket.on("jogadores",)
-  if (jogadores.primeiro === )
-  
+  // Definindo jogadores 
+  socket.on("jogadores", (jogadores))
+  if (jogadores.primeiro === socket.id) {
+    jogador = 1;
+
+  } else if (jogadores.segundo === socket.id) {
+    jogador = 2;
+
+  }  else if (jogadores.terceiro === socket.id) {
+    jogador = 3;
+
+  } else {
+     //socket.emit(“message: duplicate user”, 
+                 // “status: 401”)
+  }
+
   // Emitir Registro de jogadores p/ servidor
   this.socket.emit("Register", (jogadores) => {
-    socket.to(socketId).emit("answer", description);
-  });
+    if (jogador === 1) {
+      //criar sala
+
+    } else if (jogador === 2) {
+      // recebe convite para entrar na sala
+
+    } else if (jogador === 3) {
+      // recebe convite para entrar na sala
+      // iniciar o jogo
+
+    } else   // Quando todos os jogadores estão conectados
+      (jogadores.primeiro !== undefined && jogadores.segundo !== undefined && jogadores.terceiro !== undefined) {
+        // sala cheia, ngm pode entrar
+        // “message:  “crowded room”,
+        // “status: 403”
+        };
+    }
+  }
