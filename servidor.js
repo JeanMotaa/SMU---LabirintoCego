57570cc99534a8dd1a80f7a40c60e3cc6ea2f837
rafafonsecaa <faelafonsecaa_@outlook.com>
2022-06-22 20:18:43 +0000

    Oferta de sessão de mídia

--- servidor.js
+++ servidor.js
@@ -23,30 +23,50 @@ io.on("connection", (socket) => {
       io.to(sid).emit("register-ok", jogadores.primeiro);
       console.log(jogadores.primeiro);
     }
-    // else if (jogadores.segundo === undefined) {
-    //   var jogador = {
-    //     nome: socket.id,
-    //     id_sala: "labirintoCegoSala",
-    //     dono_sala: false,
-    //   };
-
-    //   jogadores.segundo = jogador;
-    //   // se jogador possui o acesso a sala, entra na partida
-    //   socket.join(jogador.id_sala);
-    // } else if (jogadores.terceiro === undefined) {
-    //   var jogador = {
-    //   nome: socket.id,
-    //   id_sala: "labirintoCegoSala",
-    //   dono_sala: false,
-    // };
-
-    //   jogadores.terceiro = jogador;
-    //   socket.join(jogador.id_sala);
-    // } else {
-    //   io.emit("sala_cheia");
-    // }
-
+    if (jogadores.segundo === undefined) {
+      jogadores.segundo = {
+        nome: socket.id, // alterar pois está gerando o msm nome para primeiro e segundo
+        id_sala: "labirintoCegoSala",
+        dono_sala: false,
+      };
+      socket.join(jogadores.segundo.id_sala);
+      io.to(sid).emit("register-ok", jogadores.segundo);
+      console.log(jogadores.segundo);
+    }
     io.emit("jogadores", jogadores);
+  });
+  //   jogadores.segundo = jogador;
+  //   // se jogador possui o acesso a sala, entra na partida
+  //   socket.join(jogador.id_sala);
+  // } else if (jogadores.terceiro === undefined) {
+  //   var jogador = {
+  //   nome: socket.id,
+  //   id_sala: "labirintoCegoSala",
+  //   dono_sala: false,
+  // };
+
+  //   jogadores.terceiro = jogador;
+  //   socket.join(jogador.id_sala);
+  // } else {
+  //   io.emit("sala_cheia");
+  // }
+
+  // io.emit("jogadores", jogadores);
+  //});
+
+  // Sinalização de áudio: oferta
+  socket.on("offer", (socketId, description) => {
+    socket.to(socketId).emit("offer", socket.id, description);
+  });
+
+  // Sinalização de áudio: atendimento da oferta
+  socket.on("answer", (socketId, description) => {
+    socket.to(socketId).emit("answer", description);
+  });
+
+  // Sinalização de áudio: envio dos candidatos de caminho
+  socket.on("candidate", (socketId, signal) => {
+    socket.to(socketId).emit("candidate", signal);
   });
 
   socket.on("disconnect", function () {
